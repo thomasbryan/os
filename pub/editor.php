62bdee6c0bde694fb92acb734aefbfced0c31ae0
Thomas Bryan <github@thomasbryan.info>
2017-05-01 03:10:16 -0500

    editor navigation

--- pub/editor.php
+++ pub/editor.php
@@ -1,19 +1,25 @@
-<?php $api = new API(); 
-### TODO Convert from audio into file editor
-?><!DOCTYPE html><html lang="en">
+<?php $api = new API(); ?><!DOCTYPE html><html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Text</title>
+    <title>Editor</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <style>
       body { padding-top: 70px; }
-      @media (max-width: 767px) {
-        body { padding-top: 120px; }
-      }
       .form-group { margin: 9px; }
       textarea { resize: none; }
+      .list-group:last-child { padding-right: 0; }
+      .list-group { margin-bottom: 0; }
+      .list-group a:last-child { margin-bottom: 20px; }
+      .list-group-item { cursor: pointer; overflow: hidden; white-space: nowrap; }
+      .list-group-item img { position:absolute; top:0; right:0; height: 100%; }
+      .navbar-brand { cursor: pointer; }
+      @media (max-width: 767px) {
+        body.body-edit { padding-top: 170px; }
+        .list-group { padding-right: 0; }
+      }
+      #e { position: fixed; right: 1em; z-index: 100; }
     </style>
   </head>
   <body>
@@ -21,26 +27,33 @@
       <div class="container-fluid">
         <div class="navbar-header">
           <form id="search" method="POST" >
+            <input type="hidden" name="req" value="search" />
             <div class="form-group has-feedback">
               <input type="text" id="q" name="q" class="form-control" placeholder="Search" />
               <span class="glyphicon glyphicon-search form-control-feedback"></span>
             </div>
           </form>
         </div>
-        <!-- 
-        create document in current path.
-        
-        -->
-        <div class="navbar-brand">
-        /path/to/file
+        <div class="edit hidden navbar-header">
+          <form id="edit" method="POST" >
+            <div class="form-group">
+              <input type="hidden" name="req" value="edit" />
+              <input type="hidden" id="f" name="f" />
+              <input type="text" id="n" name="n" class="form-control" placeholder="Name" />
+            </div>
+          </form>
         </div>
-        <a class="navbar-brand pull-right"><span class="glyphicon glyphicon-trash text-danger"></span> <span class="text-danger">Trash</span></a>
+        <a data-req="ce" class="edit hidden navbar-brand"><span class="glyphicon glyphicon-remove-sign"></span></a>
+        <a data-req="tn" class="edit hidden navbar-brand pull-right"><span class="glyphicon glyphicon-trash text-danger"></span> <span class='text-danger'>Trash</span></a>
       </div>
     </nav>
     <div class="container-fluid">
       <div class="row-fluid">
+        <div id="e" class="hidden alert alert-danger" role="alert"> </div>
         <div class="col-xs-12">
-          <textarea id="d" name="d" class="form-control" rows="10" placeholder="Info"></textarea>
+          <ol id="b" class="breadcrumb"><li class="active"><span class="glyphicon glyphicon-home"></span></li></ol>
+          <div id="r" class="list-group"></div>
+          <textarea id="d" name="d" class="hidden form-control" rows="10" placeholder="Info"></textarea>
         </div>
       </div>
     </div>
@@ -53,6 +66,8 @@
           if($("#q").val() != $("#q").data("q")) {
             $("#q").data("q",$("#q").val());
             $("#r").html("");
+            $("#d").addClass("hidden");
+            //breadcrumbs
           }
           if($("#r .g").length == 0 ) {
             $("#q").blur();
@@ -62,17 +77,12 @@
               data: $(this).serialize()
             }).done(function(res) {
               search(false);
-              $(".col-sm-8 h5").html("<span class='glyphicon glyphicon-search'></span> Results");
               var html = "";
-              $.each(res,function(k,v){
-                html += "<a class='g list-group-item' data-f='"+v.f+"' data-n='"+v.n+"' data-t='0'>"+v.n;
-                html += "<button class='update btn btn-default btn-xs pull-right'><span class='glyphicon glyphicon-plus-sign'></span> Playlist</button>";
-                html += "<button class='btn btn-default btn-xs pull-right'><span class='glyphicon glyphicon-pencil'></span> Edit</button>";
-                html += "</a>";
-              });
+              html += list(res);
               $("#r").html(html);
-              if($("#r .active").length==0) {$("#r a:first").addClass("active");}
+              //if($("#r .active").length==0) {$("#r a:first").addClass("active");}
             }).fail(function() {
+              search(false);
               $("#r").html("<a class='g list-group-item disabled'>Your search - <b>"+$("#q").val()+"</b> - did not match any documents.</a>");
             });
           }
@@ -80,143 +90,203 @@
           reset();
         }
       });
+      $("form#edit").on("submit",function(e) {
+        /***
+
+        update ???
+        rename file??
+
+
+        ***/
+        e.preventDefault();
+        /*
+        $.ajax({
+          type: "POST",
+          data: $(this).serialize()
+        }).fail(function() {
+          err("Unable to Update");
+        });
+        */
+        $(".edit").addClass("hidden");
+        $("body").removeClass("body-edit");
+      });
+      $(document).on("submit","form#create",function(e) {
+        /***
+        update request
+
+        empty data
+
+
+        ***/
+        e.preventDefault();
+        var n = $("#create .create").val();
+        if(n.length > 0) {
+          /*
+          $.ajax({
+            type: "POST",
+            data: "req=update&"+$(this).serialize()
+          }).done(function(res) {
+            //switch to file??
+            $("#create .create").val("");
+          }).fail(function() {
+            $("#create .create").val("");
+          });
+          */
+        }
+      });
       function search(req) {
         ( req ? $(".form-control-feedback").removeClass("glyphicon-search").addClass("glyphicon-hourglass") : $(".form-control-feedback").removeClass("glyphicon-hourglass").addClass("glyphicon-search") );
       }
-      $(document).on("click","#r .g:not(.disabled) button.update",function(e) {
-        var div = $(this)
-          , data = div.parent().data()
-          ;
-        data.l = app.l;
-        data.req = "update";
-        $.ajax({
-          type: "POST",
-          data: data
-        }).done(function(res) {
-          div.addClass("btn-info");
-        });
-        e.stopPropagation();
+      $(document).on("click","#r a:not(.disabled),#b a",function(e) {
+        app.f = $(this).data("f");
+        state();
       });
-      $(document).on("click","#r .g:not(.disabled)",function(e) {
-        play($(this).data());
-        reset();
-      });
-      $(document).on("click","#r .y:not(.disabled)",function(e) {
-        $(this).addClass("disabled").removeClass("active");
-        $.ajax({
-          type: "POST",
-          data: "req=download&d="+$(this).data("d")+"&q="+$("#q").val()
-        }).done(function(res) {
-          reset();
-        }).fail(function() {
-          //TODO check not returning something??
-          err("Failed to Download");
-          reset();
-        });
-      });
-      $(document).on("click","#r .l button",function(e) {
-        var div = $(this).parent()
-          , data = div.data()
-          ;
-        data.req = "delete";
-        $.ajax({
-          type: "POST",
-          data: data
-        }).done(function(res) {
-          div.remove();
-        }).fail(function() {
-          err("Unable to Delete '"+data.l+"' Playlist");
-        });
-        e.stopPropagation();
-      });
-      $(document).on("click","#r .l",function(e) {
-        if($(this).data("l")) {
-          app.l = $(this).data("l");
-          localStorage.app = JSON.stringify(app);
-          reset();
+      $(document).on("click",".navbar-brand",function(e) {
+        switch ($(this).data("req")) {
+          case "ce":close();break;
+          case "tn":trash();break;
         }
       });
-      $(document).on("click","#p a,#r .g:not(.disabled) button:not(.update)",function(e) {
+      $(document).on("click","#p a,#r a:not(.disabled) button:not(.update)",function(e) {
         $(".edit").removeClass("hidden");
         $("body").addClass("body-edit");
+        console.log("lets get the name of this file / directory to modify.");
+
         $("#f").val(($(this).data("f")===undefined?$(this).parent().data("f"):$(this).data("f")));
         $("#n").val(($(this).data("n")===undefined?$(this).parent().data("n"):$(this).data("n"))).focus();
+
         e.stopPropagation();
       });
-      function reset() {
-        $("#q").val("");
-        $("#r,.col-sm-8 h5").html("");
+      function close() {
+        $(".edit").addClass("hidden");
+        $("body").removeClass("body-edit");
       }
-      //var app = localStorage.app;
+      function trash() {
+        $.ajax({
+          type: "POST",
+          data: "req=delete&f="+$("#f").val()
+        }).done(function(res) {
+          $(".list-group").find("[data-f='"+$("#f").val()+"']").remove();
+          close();
+        }).fail(function() {
+          err("Failed to Delete File");
+        });
+      }
+      function err(req) {
+        $("#e").html("<strong>Error:</strong> "+req+"!").show().removeClass("hidden").delay(5000).fadeOut(500);
+      }
+      function reset() {
+        app.q = "";
+        $("#q").val("");
+        state();
+      }
+      function state() {
+        $("#b").html("<li><a href='javascript:void(0);' data-f=''><span class='glyphicon glyphicon-home'></span></a></li>");
+        if(app.f.length > 0) {
+          var b = app.f.split("/")
+            , f = ""
+            ;
+          $.each(b,function(k,v) {
+            f=f+v;
+            $("#b").append("<li><a href='javascript:void(0);' data-f='"+f+"'>"+v+"</a></li>");
+            f=f+"/";
+          });
+        }
+        $("#b li").last().html($("#b li:last a").html()).addClass("active");
+        // ??? //
+        if(app.q.length > 0) {
+          $("#q").val(app.q);
+          console.log("lets get our search on");
+        }else{
+          read();
+        }
+      }
+      function read() {
+        $.ajax({
+          type: "POST",
+          data: "req=read&f="+app.f
+        }).done(function(res) {
+          console.log(res);
+          var html = ""
+            , val = ""
+            ;
+          if($.isArray(res)) {
+            $.each(res,function(k,v) {
+              val=val+v+"\n";
+            });
+            $("#d").removeClass("hidden").val($.trim(val)).focus();
+          }else{
+            $("#d").addClass("hidden");
+            html += "<div class='list-group-item list-group-item-success'><form id='create'><div class='input-group'><input class='create form-control' name='n' type='text' placeholder='Create'><div class='input-group-btn'><button class='btn btn-default'><span class='glyphicon glyphicon-plus-sign'></span> New</button></div></div></form></div>";
+            if($.isPlainObject(res)) {
+              html += list(res);
+            }
+            //if($("#r .active").length==0) {$("#r a:first").addClass("active");}
+          }
+          $("#r").html(html);
+        });
+      }
+      function list(req) {
+        var res = "";
+        $.each(req,function(k,v){
+          res += "<a class='list-group-item' data-f='"+k+"'>"+v;
+          res += "<button class='btn btn-default btn-xs pull-right'><span class='glyphicon glyphicon-pencil'></span> Edit</button>";
+          res += "</a>";
+        });
+        return res;
+      }
+      var app = localStorage.editor;
       $(document).ready(function() {
-        /*
         if(app == null) {
-          app = {"f":"","l":"Library","n":"","p":false,"s":false,"t":0,"v":0.5};
-          fo();
+          app = {"f":"","q":""};
         }else{
           app = JSON.parse(app);
         }
-        */
-      });
-      // just set to height on load
-      $("textarea").keyup(function(e) {
-        if (e.which == 8 || e.which == 46) {
-          if( ($(this).height() + parseFloat($(this).css("font-size"))) > this.scrollHeight ) {
-            $(this).height(parseFloat($(this).css("font-size")));
-          }
-        }
-        if($('.col-xs-12').outerHeight() < ( $(window).height() - 120 ) ) {
-          while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-            $(this).height($(this).height()+1);
-          };
-        }
+        state();
       });
     </script>
   </body>
 </html><?php class API {
-  /*
-
-  TODO
-  version controled and non version controlled documents.
-  
-  ../src/txt/ < not in version control.
-  ../pub/txt/ < in version control.
-
-
-  create new file in directory X ???
-
-
-
-  */
   private $d = '';
-  private $m = 'meta';
-  private $l = 'Library';
-  private $p = './mp3/';
-  # required: youtube-dl
-  # required: libav-tools
+  private $p = '../src/';
   function __construct() {
     if($_SERVER['REQUEST_METHOD']==='POST') {
-      $this->d = dirname(__FILE__);
+      chdir($this->p);
+      $this->d = getcwd().'/';
       $res = false;
       if(!isset($_POST['req'])) $_POST['req'] = '';
       switch($_POST['req']) {
-        default: $res = $this->fetch($_POST['s'],(isset($_POST['f'])?$_POST['f']:''),(isset($_POST['l'])?$_POST['l']:''));break;
-        case 'create': $res = $this->create($_POST['l']);break;
-        case 'update': $res = $this->update($_POST['f'],$_POST['l']);break;
-        case 'delete': $res = $this->delete($_POST['l']);break;
-        case 'list': $res = $this->files($this->p,false);break;
         case 'search': $res = $this->search($_POST['q']);break;
-        case 'edit': $res = $this->edit($_POST['n'],$_POST['f']);break;
-        case 'trash': $res = $this->trash($_POST['f']);break;
+        case 'read': $res = $this->read($_POST['f']);break;
+        case 'update': $res = $this->update($_POST['f'],$_POST['d']);break;
+        case 'delete': $res = $this->delete($_POST['f']);break;
       }
       $this->json($res);
     }
   }
-  private function create($req='') {
+  private function search($req) {
     $res = false;
-    if($req!=$this->l && !empty($req) && !file_exists($this->p.$req.'.json')) {
-      file_put_contents($this->p.$req.'.json',json_encode(array()));
-      $res = true;
+    if(!empty($req)) {
+      //recurse through $this->d and $res[] = $req;
+    }
+    return $res;
+  }
+  private function read($req='') {
+    $res = false;
+    if($this->valid($req)) {
+      $req = realpath($this->d.$req);
+      if(is_dir($req)) {
+        $res = array(); 
+        $count = strlen($this->d);
+        $files = new DirectoryIterator($req);
+        foreach($files as $file) {
+          if($file->isDot()) continue;
+          $res[substr($file->getPathname(),$count)] = $file->getFilename();
+        }
+        if(empty($res)) $res=true;
+      }else{
+        $res=explode("\n",file_get_contents($req));
+      }
     }
     return $res;
   }
@@ -241,113 +311,16 @@
     }
     return $res;
   }
-  private function edit($n='',$f='') {
+  private function valid($req=false) {
     $res = false;
-    if(file_exists($this->p.$this->l.'.json')) {
-      $mp3=json_decode(file_get_contents($this->p.$this->l.'.json'),true);
-      if(isset($mp3[$f])) {
-        if(file_exists($this->p.$this->m)) $meta = json_decode(file_get_contents($this->p.$this->m),true);
-        $meta[$mp3[$f]] = $n;
-        file_put_contents($this->p.$this->m,json_encode($meta));
-        $res = true;
-      }
-    }
-    return $res;
-  }
-  private function search($req) {
-    $res = false;
-    if(!empty($req)) {
-      $mp3 = json_decode(file_get_contents($this->p.$this->l.'.json'),true);
-      if(file_exists($this->p.$this->m)) {
-        $meta = json_decode(file_get_contents($this->p.$this->m),true);
-      }
-      foreach($mp3 as $k => $v) {
-        $n = (isset($meta[$v]) ? $meta[$v] : $v);
-        if(preg_match('/'.$req.'/i',$n)) {
-          $res[] = array( 'f' => $k, 'n' => $n);
+    if(empty($req)) return true;
+    if(is_file($this->d.$req) || is_dir($this->d.$req)) {
+      $pos = strpos(realpath($this->d.$req),$this->d);
+      if($pos!==false) {
+        if($pos==0) {
+          $res = true;
         }
       }
-    }
-    return $res;
-  }
-  private function fetch($req=false,$id='',$l='') {
-    $res=false;
-    if(empty($l)) $l = $this->l;
-    if(file_exists($this->p.$l.'.json')) {
-      $mp3=json_decode(file_get_contents($this->p.$l.'.json'),true);
-    }else{
-      # ???
-      $this->refresh();
-    }
-    if(empty($id)) {
-      $id = key($mp3);
-    }else{
-      if($req == 'true') {
-        $r = array_rand($mp3,2);
-        if($r[0]!=$id) {
-          $id = $r[0];
-        }else{
-          $id = $r[1];
-        }
-      }else{
-        while(key($mp3) !== $id) next($mp3);
-        next($mp3);
-        $id = key($mp3);
-        if($id==null) $mp3 = false;
-      }
-    }
-    if($mp3) {
-      if(!file_exists($id)) {
-        unset($mp3[$id]);
-        file_put_contents($this->p.$l.'.json',json_encode($mp3));
-        return $this->fetch($req,$id,$l);
-      }
-      if(file_exists($this->p.$this->m)) $meta=json_decode(file_get_contents($this->p.$this->m),true);
-      $res = array('f' => $id, 'n' => (isset($meta[$mp3[$id]]) ? $meta[$mp3[$id]] : $mp3[$id]));
-    }
-    return $res;
-  }
-  private function trash($req) {
-    $res = false;
-    if(file_exists($req)) {
-      if(file_exists($this->p.$this->l.'.json')) {
-        $mp3=json_decode(file_get_contents($this->p.$this->l.'.json'),true);
-        if(file_exists($this->p.$this->m)) $meta = json_decode(file_get_contents($this->p.$this->m),true);
-        if(isset($meta)) unset($meta[$mp3[$req]]);
-        unlink($req);
-        if(!file_exists($req)) {
-          file_put_contents($this->p.$this->m,json_encode($meta));
-          $res = $this->refresh();
-        }
-      }
-    }
-    return $res;
-  }
-  private function files($req,$mp3=true) {
-    $res = false;
-    if(is_Dir($req)) {
-      $res = array(); 
-      $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($req));
-      if($mp3) {
-        foreach($rii as $file) {
-          if($file->isDir()|| $file->getExtension() != 'mp3') continue;
-          $res[$file->getPathname()] = $file->getBasename('.mp3'); 
-        }
-      }else{
-        foreach($rii as $file) {
-          if($file->isDir()|| $file->getExtension() != 'json') continue;
-          $res[] = $file->getBasename('.json'); 
-        }
-      }
-    }
-    return $res;
-  }
-  private function refresh() {
-    $res = false;
-    $req=$this->files($this->p);
-    if($req) {
-      file_put_contents($this->p.$this->l.'.json',json_encode($req));
-      $res = true;
     }
     return $res;
   }
