521d08a522c8b15b9f83ffb7bfa6139d8e8dec5e
Thomas Bryan <github@thomasbryan.info>
2017-09-19 15:58:00 +0000

    Update 5 files

--- pub/js/video.js
+++ pub/js/video.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    	console.log("home base");
-    var templateData = {
-      "name":"thomas",
-      "timeNow":"now",
-    };
-    $.get("htm/home.htm", function(templates) {
-      // Fetch the <script /> block from the loaded external
-      // template file which contains our greetings template.
-      var template = $(templates).filter('#tpl-home').html();
-      $("#app").html(Mustache.render(template, templateData));
-    });
+  $("#app").html("video");
 });
